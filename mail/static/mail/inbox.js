8c1fbee02f91cec30a44c65536dd729d74828602
absurdity69 <kfd@outlook.com>
2023-12-31 10:32:22 +0000

    + fetch mailbox contents and display in rows

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
@@ -8,7 +7,6 @@ document.addEventListener('DOMContentLoaded', function() {
  
   const form = document.getElementById('compose-form');
   let submitCount = 0;
-
   form.addEventListener('submit', function(event) {
     event.preventDefault();
     const recipientsInput = document.getElementById("compose-recipients");
@@ -60,6 +58,47 @@ function load_mailbox(mailbox) {
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
+  console.log('Mailbox:', mailbox);
+
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
-}
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+      // Print emails
+      console.log(emails);
+
+      const columnDiv = document.createElement('div');
+      columnDiv.classList.add('column');
+  
+      emails.forEach(email => {
+        const recipient = email.sender;
+        const subject = email.subject;
+        const timestamp = email.timestamp;
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add('row');
+        rowDiv.style.padding = '4px'; 
+        rowDiv.style.border = '1px solid black'; /
+        const recipientDiv = document.createElement('div');
+        recipientDiv.classList.add('mr-3'); 
+        const subjectDiv = document.createElement('div');
+        subjectDiv.classList.add('mr-3');
+        const timestampDiv = document.createElement('div');
+        timestampDiv.classList.add('mr-3');
+  
+        recipientDiv.textContent = recipient;
+        subjectDiv.textContent = subject;
+        timestampDiv.textContent = timestamp;
+  
+        rowDiv.appendChild(recipientDiv);
+        rowDiv.appendChild(subjectDiv);
+        rowDiv.appendChild(timestampDiv);
+  
+        columnDiv.appendChild(rowDiv);
+      });
+  
+      // Append the column to the emails view
+      document.querySelector('#emails-view').appendChild(columnDiv);
+    });  
+  }
+  
